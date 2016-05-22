16d3b5fb49aa2365ae2ef204a7344cc5a2ac6d75
dylanbeattie <dylan@dylanbeattie.net>
2016-05-22 12:58:44 +0100

    Skeleton list+upload implementation

---
+++ src/Reflexif/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reflexif.Controllers {
+    public class HomeController : Controller {
+        // GET: Home
+        public ActionResult Index() {
+            var files = Directory.GetFiles(Server.MapPath("~/Images/"), "*.jpg").Select(Path.GetFileName);
+            return View(files);
+        }
+
+        public ActionResult Upload(HttpPostedFileBase file, string title, string copyright) {
+            using (var bitmap = new Bitmap(file.InputStream)) {
+                var guid = Guid.NewGuid();
+                bitmap.Save(Path.Combine(Server.MapPath("~/Images/"), guid + ".jpg"), ImageFormat.Jpeg);
+                return (RedirectToAction("Index"));
+            }
+        }
+    }
+}
