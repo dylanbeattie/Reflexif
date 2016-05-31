d3d9306a1bf8cdf4fc712ecb70944693de114c32
dylanbeattie <dylan@dylanbeattie.net>
2016-05-31 22:12:09 +0100

    Working implementation of file upload and EXIF tag writing

--- src/Reflexif.WebDemo/Controllers/HomeController.cs
+++ src/Reflexif.WebDemo/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,10 +18,29 @@ namespace Reflexif.WebDemo.Controllers {
 
         public ActionResult Upload(HttpPostedFileBase file, string title, string copyright) {
             using (var bitmap = new Bitmap(file.InputStream)) {
-                var guid = Guid.NewGuid();
+                bitmap.SetExifTag(ExifTags.XPTitle, title);
+                bitmap.SetExifTag(ExifTags.ImageDescription, title);
+                bitmap.SetExifTag(ExifTags.XPAuthor, copyright);
+                bitmap.SetExifTag(ExifTags.Copyright, copyright);
+                var guid = DateTime.Now.Ticks.ToString("X");
+                // var guid = Guid.NewGuid();
                 bitmap.Save(Path.Combine(Server.MapPath("~/App_Data/"), guid + ".jpg"), ImageFormat.Jpeg);
                 return (RedirectToAction("Index"));
             }
+        }
+
+        public ActionResult Details(string filename) {
+            Dictionary<string, string> data = new Dictionary<String, String>();
+            using (var bitmap = new Bitmap(Path.Combine(Server.MapPath("~/App_Data/"), filename))) {
+                foreach (ExifTags tag in Enum.GetValues(typeof(ExifTags))) {
+                    try {
+                        var key = Enum.GetName(typeof(ExifTags), tag);
+                        var value = Encoding.UTF8.GetString(bitmap.GetPropertyItem((int)tag).Value);
+                        data.Add(key, value);
+                    } catch (Exception) { }
+                }
+            }
+            return (View(data));
         }
     }
 }
---
+++ src/Reflexif/Class1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Reflection;
+using System.Text;
+// ReSharper disable InconsistentNaming
+
+namespace Reflexif {
+
+    public enum ExifTags {
+        ///<summary>The name and version of the software used to post-process the picture.</summary>
+        ProcessingSoftware = 0x000b,
+        ///<summary>The name of the document from which this image was scanned</summary>
+        DocumentName = 0x010d,
+        ///<summary>A character string giving the title of the image. It may be a comment such as "1988 company picnic" or the like. Two-bytes character codes cannot be used. When a 2-bytes code is necessary, the Exif Private tag <UserComment> is to be used.</summary>
+        ImageDescription = 0x010e,
+        ///<summary>The manufacturer of the recording equipment. This is the manufacturer of the DSC, scanner, video digitizer or other equipment that generated the image. When the field is left blank, it is treated as unknown.</summary>
+        Make = 0x010f,
+        ///<summary>The model name or model number of the equipment. This is the model name or number of the DSC, scanner, video digitizer or other equipment that generated the image. When the field is left blank, it is treated as unknown.</summary>
+        Model = 0x0110,
+        ///<summary>This tag records the name and version of the software or firmware of the camera or image input device used to generate the image. The detailed format is not specified, but it is recommended that the example shown below be followed. When the field is left blank, it is treated as unknown.</summary>
+        Software = 0x0131,
+        ///<summary>The date and time of image creation. In Exif standard, it is the date and time the file was changed.</summary>
+        DateTime = 0x0132,
+        ///<summary>This tag records the name of the camera owner, photographer or image creator. The detailed format is not specified, but it is recommended that the information be written as in the example below for ease of Interoperability. When the field is left blank, it is treated as unknown. Ex.) "Camera owner, John Smith; Photographer, Michael Brown; Image creator, Ken James"</summary>
+        Artist = 0x013b,
+        ///<summary>This tag records information about the host computer used to generate the image.</summary>
+        HostComputer = 0x013c,
+        ///<summary>The name of each ink used in a separated (PhotometricInterpretation=5) image.</summary>
+        InkNames = 0x014d,
+        ///<summary>A description of the printing environment for which this separation is intended.</summary>
+        TargetPrinter = 0x0151,
+        ///<summary>ImageID is the full pathname of the original, high-resolution image, or any other identifying string that uniquely identifies the original image (Adobe OPI).</summary>
+        ImageID = 0x800d,
+        ///<summary>Copyright information. In this standard the tag is used to indicate both the photographer and editor copyrights. It is the copyright notice of the person or organization claiming rights to the image. The Interoperability copyright statement including date and rights should be written in this field; e.g., "Copyright, John Smith, 19xx. All rights reserved.". In this standard the field records both the photographer and editor copyrights, with each recorded in a separate part of the statement. When there is a clear distinction between the photographer and editor copyrights, these are to be written in the order of photographer followed by editor copyright, separated by NULL (in this case since the statement also ends with a NULL, there are two NULL codes). When only the photographer copyright is given, it is terminated by one NULL code. When only the editor copyright is given, the photographer copyright part consists of one space followed by a terminating NULL code, then the editor copyright is given. When the field is left blank, it is treated as unknown.</summary>
+        Copyright = 0x8298,
+        ///<summary>Indicates the spectral sensitivity of each channel of the camera used.</summary>
+        SpectralSensitivity = 0x8824,
+        ///<summary>The date and time when the original image data was generated.</summary>
+        DateTimeOriginal = 0x9003,
+        ///<summary>The date and time when the image was stored as digital data.</summary>
+        DateTimeDigitized = 0x9004,
+        ///<summary>Security classification assigned to the image.</summary>
+        SecurityClassification = 0x9212,
+        ///<summary>Record of what has been done to the image.</summary>
+        ImageHistory = 0x9213,
+        ///<summary>A tag used to record fractions of seconds for the DateTime tag.</summary>
+        SubSecTime = 0x9290,
+        ///<summary>A tag used to record fractions of seconds for the DateTimeOriginal tag.</summary>
+        SubSecTimeOriginal = 0x9291,
+        ///<summary>A tag used to record fractions of seconds for the DateTimeDigitized tag.</summary>
+        SubSecTimeDigitized = 0x9292,
+        ///<summary>This tag is used to record the name of an audio file related to the image data. The only relational information recorded here is the Exif audio file name and extension (an ASCII string consisting of 8 characters + '.' + 3 characters). The path is not recorded.</summary>
+        RelatedSoundFile = 0xa004,
+        ///<summary>This tag records the owner of a camera used in photography as an ASCII string.</summary>
+        CameraOwnerName = 0xa430,
+        ///<summary>This tag records the serial number of the body of the camera that was used in photography as an ASCII string.</summary>
+        BodySerialNumber = 0xa431,
+        ///<summary>This tag records the lens manufactor as an ASCII string.</summary>
+        LensMake = 0xa433,
+        ///<summary>This tag records the lens's model name and model number as an ASCII string.</summary>
+        LensModel = 0xa434,
+        ///<summary>This tag records the serial number of the interchangeable lens that was used in photography as an ASCII string.</summary>
+        LensSerialNumber = 0xa435,
+        ///<summary>Defines a unique, non-localized name for the camera model that created the image in the raw file. This name should include the manufacturer's name to avoid conflicts, and should not be localized, even if the camera name itself is localized for different markets (see LocalizedCameraModel). This string may be used by reader software to index into per-model preferences and replacement profiles.</summary>
+        UniqueCameraModel = 0xc614,
+        ///<summary>CameraSerialNumber contains the serial number of the camera or camera body that captured the image.</summary>
+        CameraSerialNumber = 0xc62f,
+        ///<summary>This tag is an ASCII string containing the name of the date/time at which the preview stored in the IFD was rendered. The date/time is encoded using ISO 8601 format.</summary>
+        PreviewDateTime = 0xc71b,
+        ///<summary>The Title property used by Windows Explorer. Windows will use ImageDescription if XPTitle is not present.</summary>
+        XPTitle = 0x9C9B,
+        /// The Subject property used by Windows Explorer. Windows will use ImageDescription if XPSubject is not present.
+        XPSubject = 0x9C9F,
+        /// <summary>The Tags property used by Windows Explorer.</summary>
+        XPKeywords = 0x9c9e,
+        /// <summary>The Comments property displayed by Windows Explorer.</summary>
+        XPComment = 0x9c9c,
+        /// <summary>The Author property used by Windows Explorer.</summary>
+        XPAuthor = 0x9C9D
+    }
+
+    public static class BitmapExtensions {
+        public static void SetExifTag(this Bitmap bitmap, ExifTags tag, string value) {
+            var t = typeof(PropertyItem);
+            var types = new Type[0];
+            var values = new object[0];
+            var ci = t.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, null, types, null);
+            var pi = (PropertyItem)ci.Invoke(values);
+            pi.Id = (int)tag;
+            pi.Type = 2;
+            var bytes = Encoding.UTF8.GetBytes(value + "\0");
+            pi.Value = bytes;
+            pi.Len = bytes.Length - 1;
+            bitmap.SetPropertyItem(pi);
+        }
+        //public static void SetCopyrightExifData(this Bitmap bitmap, string copyright) {
+        //    SetExifTag(bitmap, ExifTags.Copyright, copyright);
+        //}
+
+        //public static void SetArtistExifData(this Bitmap bitmap, string artist) {
+        //    SetExifTag(bitmap, ExifTags.Artist, artist);
+        //}
+        //public static void SetTitleExifData(this Bitmap bitmap, string title) {
+        //    SetExifTag(bitmap, ExifTags.Title, title);
+        //}
+    }
+}
