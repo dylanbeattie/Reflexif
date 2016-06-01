c9851d32e314401a28ac8e2b0d180975d5c1e2e5
dylanbeattie <dylan@dylanbeattie.net>
2016-06-01 01:51:48 +0100

    Clean up code, move types into matching files, adjust namespaces.

---
+++ src/Reflexif/BitmapExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+// ReSharper disable InconsistentNaming
+
+namespace Reflexif {
+    public static class BitmapExtensions {
+        private static Encoding GetEncoding(ExifTags tag) {
+            switch (tag) {
+                case ExifTags.XPAuthor:
+                case ExifTags.XPTitle:
+                case ExifTags.XPKeywords:
+                case ExifTags.XPComment:
+                case ExifTags.XPSubject:
+                    return (Encoding.Unicode);
+                default:
+                    return (Encoding.UTF8);
+            }
+        }
+        public static string ReadExifTag(this Bitmap bitmap, params ExifTags[] tags) {
+            foreach (var tag in tags) {
+                try {
+                    var encoding = GetEncoding(tag);
+                    return encoding.GetString(bitmap.GetPropertyItem((int)tag).Value).TrimEnd('\0');
+                } catch (Exception) {
+                    // ignored
+                }
+            }
+            return (null);
+        }
+        public static void SetExifTag(this Bitmap bitmap, ExifTags tag, string value) {
+            var t = typeof(PropertyItem);
+            var types = new Type[0];
+            var values = new object[0];
+            var ci = t.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, null, types, null);
+            var pi = (PropertyItem)ci.Invoke(values);
+            pi.Id = (int)tag;
+            pi.Type = 2;
+            var encoding = GetEncoding(tag);
+            var bytes = encoding.GetBytes(value + "\0");
+            pi.Value = bytes;
+            pi.Len = bytes.Length - 1;
+            bitmap.SetPropertyItem(pi);
+        }
+        //public static void SetCopyrightExifData(this Bitmap bitmap, string copyright) {
+        //    SetExifTag(bitmap, ExifTags.Copyright, copyright);
+        //}
+
+        //public static void SetArtistExifData(this Bitmap bitmap, string artist) {
+        //    SetExifTag(bitmap, ExifTags.Artist, artist);
+        //}
+        //public static void SetTitleExifData(this Bitmap bitmap, string title) {
+        //    SetExifTag(bitmap, ExifTags.Title, title);
+        //}
+    }
+}
--- src/Reflexif/ExifTags.cs
+++ src/Reflexif/ExifTags.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Linq.Expressions;
-using System.Reflection;
-using System.Text;
-// ReSharper disable InconsistentNaming
-
+﻿// ReSharper disable InconsistentNaming
 namespace Reflexif {
-
     public enum ExifTags {
         ///<summary>The name and version of the software used to post-process the picture.</summary>
         ProcessingSoftware = 0x000b,
@@ -79,55 +71,5 @@ namespace Reflexif {
         XPComment = 0x9c9c,
         /// <summary>The Author property used by Windows Explorer.</summary>
         XPAuthor = 0x9C9D
-    }
-
-    public static class BitmapExtensions {
-        private static Encoding GetEncoding(ExifTags tag) {
-            switch (tag) {
-                case ExifTags.XPAuthor:
-                case ExifTags.XPTitle:
-                case ExifTags.XPKeywords:
-                case ExifTags.XPComment:
-                case ExifTags.XPSubject:
-                    return (Encoding.Unicode);
-                default:
-                    return (Encoding.UTF8);
-            }
-        }
-        public static string ReadExifTag(this Bitmap bitmap, params ExifTags[] tags) {
-            foreach (var tag in tags) {
-                try {
-                    var encoding = GetEncoding(tag);
-                    return encoding.GetString(bitmap.GetPropertyItem((int)tag).Value).TrimEnd('\0');
-                } catch (Exception) {
-                    // ignored
-                }
-            }
-            return (null);
-        }
-        public static void SetExifTag(this Bitmap bitmap, ExifTags tag, string value) {
-            var t = typeof(PropertyItem);
-            var types = new Type[0];
-            var values = new object[0];
-            var ci = t.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, null, types, null);
-            var pi = (PropertyItem)ci.Invoke(values);
-            pi.Id = (int)tag;
-            pi.Type = 2;
-            var encoding = GetEncoding(tag);
-            var bytes = encoding.GetBytes(value + "\0");
-            pi.Value = bytes;
-            pi.Len = bytes.Length - 1;
-            bitmap.SetPropertyItem(pi);
-        }
-        //public static void SetCopyrightExifData(this Bitmap bitmap, string copyright) {
-        //    SetExifTag(bitmap, ExifTags.Copyright, copyright);
-        //}
-
-        //public static void SetArtistExifData(this Bitmap bitmap, string artist) {
-        //    SetExifTag(bitmap, ExifTags.Artist, artist);
-        //}
-        //public static void SetTitleExifData(this Bitmap bitmap, string title) {
-        //    SetExifTag(bitmap, ExifTags.Title, title);
-        //}
     }
 }
